51b4b594ce8d2f01f500b6bf6fabc3dd741b8a5c
Carlos Augusto Porto Freitas <carlos.portof@hotmail.com>
2025-12-21 04:19:52 -0300

    fsat2-engine: Add capture task for engine

--- src/fsat2-engine.c
+++ src/fsat2-engine.c
@@ -7,6 +7,54 @@
 #include <assert.h>
 #include <pthread.h>
 
+const char *sub_str = "tcp://0.0.0.0:2808";
+const char *pub_str = "tcp://0.0.0.0:2809";
+
+static void *capture_task(void *ctx)
+{
+	int ret;
+
+	printf("Capture/logging task listening on %s\n", sub_str);
+
+	/* Subscriber (RX) */
+	void *subscriber = zmq_socket(ctx, ZMQ_SUB);
+	ret = zmq_connect(subscriber, pub_str);
+	if (ret < 0) {
+		perror("Unable to connect");
+		exit(1);
+	}
+	ret = zmq_setsockopt(subscriber, ZMQ_SUBSCRIBE, "", 0);
+	if (ret < 0) {
+		perror("Failed to call setsockopt");
+		exit(1);
+	}
+
+	while (1) {
+		zmq_msg_t msg;
+		zmq_msg_init_size(&msg, 8192);
+
+		/* Receive data */
+		if (zmq_msg_recv(&msg, subscriber, 0) < 0) {
+			zmq_msg_close(&msg);
+			printf("ZMQ Err: %s\n", zmq_strerror(zmq_errno()));
+			continue;
+		}
+
+		size_t datalen = zmq_msg_size(&msg);
+
+		uint8_t *rx_data = zmq_msg_data(&msg);
+
+		printf("XSUB: ");
+
+		for (size_t i = 0; i < datalen; ++i) {
+			printf("%#X, ", rx_data[i]);
+		}
+		printf("\n\r");
+
+		zmq_msg_close(&msg);
+	}
+}
+
 int main(int argc, char **argv)
 {
 	void *ctx = zmq_ctx_new();
@@ -23,7 +71,7 @@ int main(int argc, char **argv)
 		exit(EXIT_FAILURE);
 	}
 
-	assert(zmq_bind(xsub, "tcp://*:2808") == 0);
+	assert(zmq_bind(xsub, sub_str) == 0);
 
 	void *xpub = zmq_socket(ctx, ZMQ_XPUB);
 
@@ -32,9 +80,12 @@ int main(int argc, char **argv)
 		exit(EXIT_FAILURE);
 	}
 
-	assert(zmq_bind(xpub, "tcp://*:2809") == 0);
+	assert(zmq_bind(xpub, pub_str) == 0);
 
 	fprintf(stdout, "Started FlatSat2 Engine Proxy - xsub[*:2808] - xpub[*:2809]\n");
+
+	pthread_t capworker;
+	pthread_create(&capworker, NULL, capture_task, ctx);
 
 	zmq_proxy(xsub, xpub, NULL);
 
